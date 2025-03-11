28cb028ee25b69c80ca65408c576de072a6ad2c5
Oliwia <netirin@gmail.com>
2025-03-11 19:58:55 +0100

    Rewrote Powershell start sript to initialize venv

--- start.ps1
+++ start.ps1
@@ -1,4 +1,7 @@
 Param($ftp_pass, $nextcloud_pass, $notion_token, $page_id, $email_db, $contact_db)
-.\venv\Scripts\python.exe .\set_env.py --ftp_pass=$ftp_pass --nextcloud_pass=$nextcloud_pass --notion_token=$notion_token --page_id=$page_id --email_db=$email_db --contact_db=$contact_db
+python -m venv venv
+.\venv\Scripts\activate
+pip install -r requirements.txt
+python .\set_env.py --ftp_pass=$ftp_pass --nextcloud_pass=$nextcloud_pass --notion_token=$notion_token --page_id=$page_id --email_db=$email_db --contact_db=$contact_db
 docker compose up -d
-.\venv\Scripts\python.exe .\main.py
+python .\main.py
