1b0af0fc77f0c97c1643f1eff8083b1b48e4e7ad
Oliwia <netirin@gmail.com>
2025-03-14 20:11:14 +0100

    Now skipping email file after 3 upload retries. Archiving files to timestamped directories to prevent files replacing files of the same name.

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 import random, string
+import time
+import ftplib
 
 def randomword(length):
    letters = string.ascii_lowercase
@@ -6,3 +8,8 @@ def randomword(length):
 
 def chunkstring(string, length):
     return (string[0+i:length+i] for i in range(0, len(string), length))
+
+def move_to_dated_folder(ftp:ftplib.FTP, filename, dir):
+   folder_name = time.strftime('%Y%m%d%H%M%S')
+   path = ftp.mkd("../" + dir + "/" + folder_name)
+   ftp.rename(filename, "/" + path + "/" + filename)
--- main.py
+++ main.py
@@ -1,54 +1,61 @@
+from datetime import datetime
 import ftplib, os
 from time import sleep
 import schedule
 from dotenv import load_dotenv
 from eml_parser import parse_eml
 from notion import write_email_to_notion, write_contact_to_notion,create_email_database,create_contacts_database
+from helpers import move_to_dated_folder
 
 load_dotenv()
 
 def job():
     create_email_database(os.environ["EMAIL_DB_NAME"])
     create_contacts_database(os.environ["CONTACT_DB_NAME"])
-    retry = True
-    while (retry):
-        try:
-            #Connection to ftp server 
-            ftp = ftplib.FTP(os.environ["FTP_HOST"], os.environ["FTP_USER"], os.environ["FTP_SECRET"]) 
-            #Move to foler with files 
-            ftp.cwd(os.environ["FTP_FILE_FOLDER"])
-            #Get list of files
-            filenames = ftp.nlst()
-            retry = False
-            if len(filenames) > 0:
-                for filename in filenames:
-                    data=[]
-                    file = []
-                    #Get file data as binary
-                    ftp.retrbinary('RETR '+ filename, file.append)
-                    #Parse decoded data as bytes
-                    data = parse_eml(b''.join(file).decode())
-                    write_email_to_notion(data)
-                    write_contact_to_notion(data)
-                    #Move file to arhcive
-                    ftp.rename(filename, os.environ["FTP_ARCHIVE_FOLDER_PATH"]+'/'+filename)
-                    
-            else:
-                print("No files to download.")
-            ftp.quit()
-        except ftplib.error_perm as e:
-            print("An error occured: %s", e)
-            print("Quitting")
-            retry=False
-        except Exception as e:
-            print("An error occured: %s", e)
-            print("Retrying...")
-            sleep(10)
-            retry = True
+    try:
+        #Connection to ftp server 
+        ftp = ftplib.FTP(os.environ["FTP_HOST"], os.environ["FTP_USER"], os.environ["FTP_SECRET"]) 
+        #Move to foler with files 
+        ftp.cwd(os.environ["FTP_FILE_FOLDER"])
+        #Get list of files
+        filenames = ftp.nlst()
+        if len(filenames) > 0:
+            for filename in filenames:
+                data=[]
+                file = []
+                retry = 0
+                #Get file data as binary
+                ftp.retrbinary('RETR '+ filename, file.append)
+                #Parse decoded data as bytes
+                dir = "archive"
+                while retry < 3:
+                    try:
+                        data = parse_eml(b''.join(file).decode())
+                        write_email_to_notion(data)
+                        write_contact_to_notion(data)
+                        #Move file to arhcive
+                        retry = 4
+                    except Exception as e:
+                        retry += 1
+                        if retry == 3:
+                            print("Couldn't upload email file "+ filename)
+                            dir = "skipped"
+                        print("An error occured: %s", e)
+                        print("Retrying...")
+                        sleep(3)
+                move_to_dated_folder(ftp, filename, dir)
+
+        else:
+            print("No files to download.")
+        ftp.quit()
+    except ftplib.error_perm as e:
+        print("An error occured: %s", e)
+        print("Quitting")
+        exit(1)
 
 if __name__ == "__main__":
         print("WORKING")
-        schedule.every(30).seconds.do(job)
+        schedule.every(5).seconds.do(job)
         while True:
             schedule.run_pending()
             sleep(1)
